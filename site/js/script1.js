5072130302f0ef57f8f226b7d4bd1a0f868c958d
yes333 <sanyu.ye@outlook.com>
2021-02-12 01:04:02 +0100

    second try

--- site/js/script1.js
+++ site/js/script1.js
@@ -97,9 +97,7 @@ $(function () {
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-
                 var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-                
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
                 // chosen category from STEP 2. Use existing insertProperty function for that purpose.
                 // Look through this code for an example of how to do use the insertProperty function.
@@ -114,7 +112,7 @@ $(function () {
                 var homeHtmlToInsertIntoMainPage = insertProperty(
                     homeHtml,
                     "randomCategoryShortName",
-                    chosenCategoryShortName
+                    "L" //chosenCategoryShortName
                 );
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
